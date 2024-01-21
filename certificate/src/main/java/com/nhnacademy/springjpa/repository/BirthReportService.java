b1b35b55e20bf9938be8d557d713537ea9429843
{psm4171} <{kkjjhh4171@naver.com}>
2024-01-22 00:19:05 +0900

    feat: BirthReportService

--- certificate/src/main/java/com/nhnacademy/springjpa/repository/BirthReportService.java
+++ certificate/src/main/java/com/nhnacademy/springjpa/repository/BirthReportService.java
@@ -1,4 +1,10 @@
 package com.nhnacademy.springjpa.repository;
 
+import com.nhnacademy.springjpa.domain.BirthReportDTO;
+import com.nhnacademy.springjpa.entity.BirthDeathReportResident;
+
 public interface BirthReportService {
+    BirthDeathReportResident registerBirthReport(BirthReportDTO birthReportDTO);
+    BirthDeathReportResident modifyBirthReport(Long targetSerialNumber, BirthReportDTO birthReportDTO);
+    void deleteBirthReport(Long targetSerialNumber);
 }
